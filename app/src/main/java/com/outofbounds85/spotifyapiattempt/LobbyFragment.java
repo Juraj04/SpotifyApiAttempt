e269a7ee4690207f35c2c3d842590f799ed9899d
Janik <janik.juraj2@gmail.com>
2017-11-25 17:00:39 +0200

    lobby fragment created

---
+++ app/src/main/java/com/outofbounds85/spotifyapiattempt/LobbyFragment.java
@@ -0,0 +1,43 @@
+package com.outofbounds85.spotifyapiattempt;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LobbyFragment extends Fragment {
+
+    public LobbyFragment() {
+        // Required empty public constructor
+    }
+
+        @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lobby, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
